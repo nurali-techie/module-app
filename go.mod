d2dbb77b6b2ef46922e76759a0d32cddaf83f342
nurali-techie <nurali.techie@gmail.com>
2020-06-20 12:16:18 +0530

    step5 - Using GenEmail from v2.0.0 but Using UserName from v1.1.0

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nurali-techie/module-app
 
 go 1.14
 
-require github.com/nurali-techie/module-demo v1.1.0
+require (
+	github.com/nurali-techie/module-demo v1.1.0
+	github.com/nurali-techie/module-demo/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 
 	demo "github.com/nurali-techie/module-demo"
+	demoV2 "github.com/nurali-techie/module-demo/v2"
 )
 
 func main() {
-	userName := demo.UserName(" Nurali ", " Virani ")
+	userName := demo.UserName("Nurali", "Virani")
 	fmt.Printf("userName=%s\n", userName)
 
 	password := demo.GeneratePassword()
 	fmt.Printf("password=%s\n", password)
+
+	email := demoV2.GenerateEmail(userName)
+	fmt.Printf("email=%s\n", email)
 }
